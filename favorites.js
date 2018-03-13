9c103664b5e691ec9581be8e10dbbd30eef6ace5
kcleland <kevin.cleland@gmail.com>
2018-03-13 04:13:30 +0000

    5th commit

--- favorites.js
+++ favorites.js
@@ -11,4 +11,27 @@ $(document).ready(function() {
     $("#brain-rules-insights-btn").click(function() {
         $("#brain-rules-insights").fadeToggle("slow");
     });
+    
+    // TAGS
+    // $("#fiction-tag").click(function() {
+    //   $('.non-fiction').css({
+    //       "display": "none",
+    //       "opacity": "0.4"
+    //   }); 
+    //   $('.fiction').css({"display": "block"});
+    // });
+    // $("#non-fiction-tag").click(function() {
+    //     $('.fiction').css({
+    //         "display": "none",
+    //       "opacity": "0.4"
+    //     }); 
+    //   $('.non-fiction').css({"display": "block"});
+    // });
+    
+    $('.tag').click(function() {
+        $('.post').addClass("inactive-post");
+        var tag_clicked = $(this).attr("id");
+        $('.' + tag_clicked).removeClass("inactive-post").addClass("active-post");
+    });
+    
 })
